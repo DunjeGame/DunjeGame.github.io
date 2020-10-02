79c565af8444d7f4a962c809ebe021bd3ed95055
DunjeGame <42189233+DunjeGame@users.noreply.github.com>
2020-10-02 23:58:29 +0200

    Update scriptprivacypolicy.js

--- scriptprivacypolicy.js
+++ scriptprivacypolicy.js
@@ -1,11 +1,33 @@
 console.log("ScriptPrivacyPolicy");
+if ("ontouchstart" in document.documentElement)
+{
+  alert("handy")
+console.log("your device is a touch screen device.");
+document.write("your device is a touch screen device.");
+}
+else
+{
+ console.log("your device is a touch screen device.");
+ document.write("your device is NOT a touch device");
+}
 document.addEventListener("click", someFunction);
 function someFunction(event) {
+  console.log("function someFunction");
   console.log(event.target.id);
   if (event.target.id == 'support') {console.log("Click sur support");window.location.href = "https://dunjegame.github.io/Support.html";return;}
   if (event.target.id == 'titre4') {console.log("Click sur titre4");window.location.href = "https://dunjegame.github.io/Support.html";return;}
-  if (event.target.id == 'store') {console.log("Click sur privacy");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
-  if (event.target.id == 'titre3') {console.log("Click sur titre5");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'store') {console.log("Click sur store	");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'titre3') {console.log("Click sur titre3");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  console.log(event.target.id);
+}
+document.addEventListener("touchstart", touchFunction);
+function touchFunction(event) {
+  console.log("function touchFunction");
+  console.log(event.target.id);
+  if (event.target.id == 'support') {console.log("Click sur support");window.location.href = "https://dunjegame.github.io/Support.html";return;}
+  if (event.target.id == 'titre4') {console.log("Click sur titre4");window.location.href = "https://dunjegame.github.io/Support.html";return;}
+  if (event.target.id == 'store') {console.log("Click sur store	");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'titre3') {console.log("Click sur titre3");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
   console.log(event.target.id);
 }
 
