5278f7b322bee384317782146adc0ad535698c30
DunjeGame <42189233+DunjeGame@users.noreply.github.com>
2020-10-05 15:58:40 +0200

    Update scriptstore.js

--- scriptstore.js
+++ scriptstore.js
@@ -1,12 +1,19 @@
-console.log("ScriptStore");
+console.log("ScriptSupport");
 document.addEventListener("click", someFunction);
 function someFunction(event) {
-  console.log("function someFunction");
   console.log(event.target.id);
   if (event.target.id == 'privacy') {console.log("Click sur privacy");window.location.href = "https://dunjegame.github.io/PrivacyPolicy.html";return;}
   if (event.target.id == 'titre5') {console.log("Click sur titre5");window.location.href = "https://dunjegame.github.io/PrivacyPolicy.html";return;}
-  if (event.target.id == 'support') {console.log("Click sur support");window.location.href = "https://dunjegame.github.io/Support.html";return;}
-  if (event.target.id == 'titre4') {console.log("Click sur titre4");window.location.href = "https://dunjegame.github.io/Support.html";return;}
+  if (event.target.id == 'store') {console.log("Click sur store");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'titre3') {console.log("Click sur titre3");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'badgeApp1') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/4-in-a-row-board-game/id1067283885?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp2') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/battleship-board-game/id1202820233?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp3') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/war-card-game-for-two-players/id997498692?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp4') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/the-game-of-snakes-and-ladders/id1056804355?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp5') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/ludo-horse-racing-game/id896271238?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp6') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/game-of-the-goose-classic/id979366117?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp7') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/color-code-board-game/id1127260440?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp8') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/captain-memo-game/id817586038?itsct=apps_box&itscg=30200";return;}
   console.log(event.target.id);
 }
 document.addEventListener("touchstart", touchFunction);
@@ -15,8 +22,16 @@ function touchFunction(event) {
   console.log(event.target.id);
   if (event.target.id == 'privacy') {console.log("Click sur privacy");window.location.href = "https://dunjegame.github.io/PrivacyPolicy.html";return;}
   if (event.target.id == 'titre5') {console.log("Click sur titre5");window.location.href = "https://dunjegame.github.io/PrivacyPolicy.html";return;}
-  if (event.target.id == 'support') {console.log("Click sur support");window.location.href = "https://dunjegame.github.io/Support.html";return;}
-  if (event.target.id == 'titre4') {console.log("Click sur titre4");window.location.href = "https://dunjegame.github.io/Support.html";return;}
+  if (event.target.id == 'store') {console.log("Click sur store");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'titre3') {console.log("Click sur titre3");window.location.href = "https://dunjegame.github.io/GameStore.html";return;}
+  if (event.target.id == 'badgeApp1') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/4-in-a-row-board-game/id1067283885?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp2') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/battleship-board-game/id1202820233?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp3') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/war-card-game-for-two-players/id997498692?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp4') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/the-game-of-snakes-and-ladders/id1056804355?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp5') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/ludo-horse-racing-game/id896271238?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp6') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/game-of-the-goose-classic/id979366117?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp7') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/color-code-board-game/id1127260440?itsct=apps_box&itscg=30200";return;}
+  if (event.target.id == 'badgeApp8') {console.log("Click sur titre5");window.location.href = "https://apps.apple.com/us/app/captain-memo-game/id817586038?itsct=apps_box&itscg=30200";return;}
   console.log(event.target.id);
 }
 
